6d9ce2640f7ebfc46718de1b10cb17ed0dfea57b
Soroush <soroush.bassam@gmail.com>
2020-07-11 14:31:57 -0400

    added model

--- app.py
+++ app.py
@@ -5,22 +5,29 @@ import os
 import time
 import io
 
+import torch
+from spacy.util import load_model
+from transformers import T5Tokenizer, T5ForConditionalGeneration
+
+
+
 def main():
-    load_model()
     st.title('Nub 1.0 Summarizer')
     input_text = st.text_area("Enter text")
     'or...'
     uploaded_file = st.file_uploader("Choose a text file", type="txt")
+    tokenizer, model = load_model()
     if st.button('Summarize'):
         if uploaded_file and not input_text:
             text_from_file = process_file(uploaded_file)
-            run_summarizer(text_from_file)
+            run_summarizer(text_from_file, tokenizer, model)
         else:
             if input_text:
-                run_summarizer(input_text)
+                run_summarizer(input_text, tokenizer, model)
             else:
                 'gotta give *some* input'
     #todo: add radio button
+
 
 
 
@@ -39,18 +46,29 @@ def process_file(uploaded_file):
 
     return input_text
 
-@st.cache
+@st.cache()
 def load_model():
-    pass
+    # output_dir = '/Users/soroush/Documents/nub-summarizer/model'
+    model = T5ForConditionalGeneration.from_pretrained('soroush/model')
+    tokenizer = T5Tokenizer.from_pretrained('soroush/model')
+    return tokenizer, model
 
-def run_summarizer(input_text):
 
+def run_summarizer(input_text, tokenizer, model):
 
     'brb...'
 
-    # Add a placeholder
     latest_iteration = st.empty()
     bar = st.progress(0)
+    inputs = tokenizer.encode("summarize: " + input_text, return_tensors="pt", max_length=512)
+    outputs = model.generate(
+        inputs,
+        max_length=150,
+        length_penalty=1.0,
+        num_beams=2,
+        repetition_penalty=2.5,
+        early_stopping=True)
+    output_text = tokenizer.decode(outputs[0])
 
     for i in range(100):
         # Update the progress bar with each iteration.
@@ -58,9 +76,11 @@ def run_summarizer(input_text):
         bar.progress(i + 1)
         time.sleep(0.01)
 
+
     '...and here\'s the summary'
-    st.write(input_text)
     bar.empty()
+    st.write(output_text)
+
 
 
 
